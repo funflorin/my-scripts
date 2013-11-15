09775a88ca36872e012d13d1345ec6d7b7f86aff
Florin Capatina <florin.capatina@gmail.com>
2013-11-15 19:32:38 +0200

    Added function for showing all combination.

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+function afisare_combinari($a, $len){
+	if ($len > count($a)) return 'error';
+	$out = array();
+	if ($len==1) {
+		foreach ($a as $v) $out[] = array($v);
+		return $out;
+	}
+	$len--;
+	while (count($a) > $len) {
+		$b = array_shift($a);
+		$c = afisare_combinari($a, $len);
+		foreach ($c as $v){
+			array_unshift($v, $b);
+			$out[] = $v;
+		}
+	}
+	return $out;
+}
